18e00236ebe2c8d1642e5627d8dda090bb1643d9
FFF2832 <90552202+FFF2832@users.noreply.github.com>
2022-06-10 17:53:21 +0800

    Delete mainl9.js

--- js/mainl9.js
+++
@@ -1,6 +0,0 @@
-
-$(window).on('activate.bs.scrollspy',function(e){
-    console.log(e.relatedTarget);
-    $('.article-title').removeClass('article-title-active');
-    $(e.relatedTarget+' .article-title').addClass('article-title-active');
-});
